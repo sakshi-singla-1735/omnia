b03e0dc60af1a5ddf483b8fc287a3485295d29b4
sakshi-singla-1735 <sakshi.s@dell.com>
2025-07-22 09:06:13 +0000

    chnaging vault path

--- common/library/module_utils/input_validation/validation_flows/common_validation.py
+++ common/library/module_utils/input_validation/validation_flows/common_validation.py
@@ -1113,7 +1113,7 @@ def process_encrypted_file(secret_file_path,vault_secret_file_path,errors):
 def validate_powerscale_secret_and_values_file(secret_file_path, values_file_path,errors):
     #valiadte secret file inputs
     secrets_file_encrypted = validation_utils.is_file_encrypted(secret_file_path)
-    vault_secret_file_path= "/root/omnia/scheduler/roles/k8s_csi_powerscale_plugin/files/.csi_powerscale_secret_vault"
+    vault_secret_file_path= "/omnia/scheduler/roles/k8s_csi_powerscale_plugin/files/.csi_powerscale_secret_vault"
     #check if secret file exists
     file_exists = os.path.exists(vault_secret_file_path.strip())
 
