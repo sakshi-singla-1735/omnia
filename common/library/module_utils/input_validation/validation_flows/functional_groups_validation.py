98e8a38a8d8425c6bb8c19d49ec81b83d689c8f5
Milisha-Gupta <milisha.gupta@dell.com>
2025-09-22 06:38:42 +0000

    input validation changes

--- common/library/module_utils/input_validation/validation_flows/functional_groups_validation.py
+++ common/library/module_utils/input_validation/validation_flows/functional_groups_validation.py
@@ -1,6 +1,18 @@
-"""
-This module contains functions for validating functional_groups_config.yml
-"""
+# Copyright 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=import-error,no-name-in-module,too-many-positional-arguments,too-many-arguments
 
 from ansible.module_utils.input_validation.common_utils import validation_utils
 from ansible.module_utils.input_validation.common_utils import config
