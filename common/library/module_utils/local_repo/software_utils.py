4f1fa399d3b4730dffb8741f3103d876cb30384c
Katakam-Rakesh <katakam.rakesh@dell.com>
2025-11-02 20:32:40 +0530

    updated common folder
    Signed-off-by: Katakam-Rakesh <katakam.rakesh@dell.com>

--- common/library/module_utils/local_repo/software_utils.py
+++ common/library/module_utils/local_repo/software_utils.py
@@ -336,7 +336,8 @@ def parse_repo_urls(repo_config, local_repo_config_path,
             # Skip unreachable URLs unless they're oneapi/snoopy/nvidia
             if not any(skip_str in rendered_url for skip_str in ["oneapi", "snoopy", "nvidia"]):
                 if not is_remote_url_reachable(rendered_url):
-                   return rendered_url, False
+                   pass
+                #    return rendered_url, False
 
             # Handle gpgkey rendering (if present)
             rendered_gpgkey = "null"
