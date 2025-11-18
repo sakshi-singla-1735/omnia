5eb7a4140a707680417ac0b0ba0c2ac53e5f213b
Jagadeesh N V <jagadeesh_n_v@dell.com>
2025-11-18 14:45:46 +0530

    slurm _onf sanity fixed

--- common/library/modules/slurm_conf.py
+++ common/library/modules/slurm_conf.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python
 
 from collections import OrderedDict
-import json
 from ansible.module_utils.basic import AnsibleModule
 import os
-import configparser
 
 # NOTE: depends on python3.7+ where dict order is maintained
 
+
 def read_dict2ini(conf_dict):
     data = []
-    for k,v in conf_dict.items():
+    for k, v in conf_dict.items():
         if isinstance(v, list):
             for item in v:
                 if isinstance(item, dict):
-                    data.append(" ".join(f"{key}={value}" for key, value in item.items()))
+                    data.append(
+                        " ".join(f"{key}={value}" for key, value in item.items()))
                 else:
                     data.append(f"{k}={item}")
         else:
             data.append(f"{k}={v}")
 
     return data
+
 
 def parse_slurm_conf(file_path, module):
     """Parses the slurm.conf file and returns it as a dictionary."""
@@ -29,12 +30,13 @@ def parse_slurm_conf(file_path, module):
 
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"{file_path} not found.")
-    
+
     try:
         with open(file_path, 'r') as f:
             for line in f:
-                line = line.split('#')[0].strip() # handles any comment after the data
-                
+                # handles any comment after the data
+                line = line.split('#')[0].strip()
+
                 # Skip comments and empty lines
                 if not line:
                     continue
@@ -58,18 +60,14 @@ def parse_slurm_conf(file_path, module):
                         key, value = item.split('=')
                         tmp_dict[key.strip()] = value.strip()
                     if tmp_dict:
-                        # module.warn("1")
-                        # module.warn(list(tmp_dict.keys())[0])
-                        # module.warn("2")
-                        # module.warn(json.dumps(slurm_dict))
-                        # module.warn("3")
-                        # module.warn(json.dumps(slurm_dict.get(list(tmp_dict.keys())[0], [])))
-                        slurm_dict[list(tmp_dict.keys())[0]] = list(slurm_dict.get(list(tmp_dict.keys())[0], [])) + [tmp_dict]
+                        slurm_dict[list(tmp_dict.keys())[0]] = list(
+                            slurm_dict.get(list(tmp_dict.keys())[0], [])) + [tmp_dict]
 
     except Exception as e:
         raise Exception(f"Error reading or parsing {file_path}: {str(e)}")
 
     return slurm_dict
+
 
 def run_module():
     module_args = dict(
@@ -95,20 +93,18 @@ def run_module():
         if module.params['op'] == 'f2d':
             s_dict = parse_slurm_conf(module.params['path'], module)
             result['slurm_dict'] = s_dict
-        else: #d2f
+        else:  # d2f
             s_list = read_dict2ini(module.params['conf_map'])
             result['slurm_conf'] = s_list
-        # result['f1'] = s_dict[list(s_dict.keys())[0]]
-        # result['node20'] = s_dict['NodeName'][1][list(s_dict['NodeName'][1].keys())[0]]
-        # result['config_parsed'] = read_ini_file(module.params['path'])
     except Exception as e:
         result['failed'] = True
         result['msg'] = str(e)
 
         module.fail_json(msg=str(e))
-    
+
     # Return the result
     module.exit_json(**result)
+
 
 if __name__ == '__main__':
     run_module()
